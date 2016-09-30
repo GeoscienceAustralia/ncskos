9fbf640713e5dbf8a8a1a18e478c8728ea53a86a
Alex Ip <alex@trentham.net.au>
2016-09-30 12:18:49 +1000

    First working commit

---
+++ nclddump/__main__.py
@@ -0,0 +1,13 @@
+'''
+Created on 30 Sep 2016
+
+@author: Alex Ip
+'''
+import sys
+from nclddump import NCLDDump
+
+def main():
+    NCLDDump(sys.argv[1:])
+    
+if __name__ == '__main__':
+    main()
