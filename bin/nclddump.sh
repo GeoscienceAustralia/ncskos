c6c79f659817b3ea8c289269d3b6dd680f9d376a
Alex Ip <alex@trentham.net.au>
2016-10-04 19:41:45 +1100

    DOS EOL fixed. Bad PYTHONPATH fixed.

--- bin/nclddump.sh
+++ bin/nclddump.sh
@@ -4,6 +4,7 @@
 # Example invocation: nclddump.sh -hs /home/547/axi547/sst.ltm.1971-2000_skos.nc --skos lang=pl altLabels=True narrower=True broader=True
 
 # Assume script is in bin directory under module directory
-export PYTHONPATH=$(dirname $(readlink -f "$0")):%PYTHONPATH%
+export PYTHONPATH=$(dirname $(dirname $(readlink -f "$0"))):$PYTHONPATH
+echo $PYTHONPATH
 
 python -m nclddump $@
