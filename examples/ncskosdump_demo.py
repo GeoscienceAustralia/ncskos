9692181111b8a9b252797e86d4708324399cfbe0
Alex Ip <alex@trentham.net.au>
2016-12-18 11:31:25 +1100

    Output enhanced

--- examples/ncskosdump_demo.py
+++ examples/ncskosdump_demo.py
@@ -31,15 +31,19 @@ def main():
             arg_value[key_value[0]] = key_value[1]
         
     # Default to project "data" directory
-    data_dir = (arg_value.get('dataset_dir') or 
-                os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'data')
-                )
+    if not arg_value.get('dataset_dir'):
+        arg_value['dataset_dir'] = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'data')
+        
+    print 'Parameters:'
+    for arg_key in arg_value.keys():
+        print '  %s = %s' % (arg_key, arg_value[arg_key])
     
     concept_hierarchy = ConceptHierarchy(lang=arg_value.get('lang'))   
      
-    nc_path_list = sorted(glob(os.path.join(data_dir, '*.nc')))
+    nc_path_list = sorted(glob(os.path.join(arg_value['dataset_dir'], '*.nc')))
 
     dataset_var_concept_dict = {}
+    print '\nReading %s netCDF files' % len(nc_path_list)
     for nc_path in nc_path_list:
         nc_dataset = netCDF4.Dataset(nc_path, 'r')
         
@@ -47,11 +51,11 @@ def main():
         for data_variable_name in sorted(data_variable_names):
             data_variable = nc_dataset.variables[data_variable_name]
             concept_uri = data_variable.__dict__.get('skos__concept_uri')
-            print 'Processing ' + os.path.basename(nc_path) + ':' + data_variable_name + ' with URI', concept_uri
+            print '  Processing ' + os.path.basename(nc_path) + ':' + data_variable_name + ' with URI', concept_uri
             concept = concept_hierarchy.get_concept(concept_uri)
             dataset_var_concept_dict[(nc_path, data_variable_name)] = concept
 
-    print
+    print '=' * 80 + '\n'
     for top_concept in [concept for concept in concept_hierarchy.get_top_concepts()]:
         print_concept_tree(top_concept, dataset_var_concept_dict)
         
