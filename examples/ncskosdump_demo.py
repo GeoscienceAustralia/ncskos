8c19998761d567817661fd2b45d1988edfe6b100
Alex Ip <alex@trentham.net.au>
2016-12-19 13:01:16 +1100

    Parameters rejigged to use leading '-' for switches "narrower", "broader" and "verbose" options implemented

--- examples/ncskosdump_demo.py
+++ examples/ncskosdump_demo.py
@@ -5,6 +5,7 @@ Created on 16Dec.,2016
 '''
 import sys
 import os
+import re
 from glob import glob
 import netCDF4
 
@@ -12,9 +13,12 @@ from ncskosdump.concept_hierarchy import ConceptHierarchy
 
 def main():
     '''
-    Function to read concept URIs from all data variables in all netCDF files in dataset directory and
-    display them in a concept hierarchy.
-    Note that language can be specified using command line argument "lang=<lang_code>"
+    Function to read concept URIs from all data variables in netCDF files and display them in a 
+    concept hierarchy.
+    Note that language can be specified using command line argument "--lang=<lang_code>"
+    Concept trees can be recursively populated down to bottom concepts if "--narrower" command line argument is used
+    Targets specified on the command line can be either individual netCDF files or directories 
+    containing netCDF files.
     Dataset directory defaults to project "data" directory adjacent to directory containing this script
     '''
     def print_concept_tree(concept, dataset_var_concept_dict, level=0):
@@ -28,27 +32,47 @@ def main():
             print_concept_tree(narrower_concept, dataset_var_concept_dict, level+1)
 
     # Resolve "<key>=<value>" arguments. Assume key-only argument is dataset directory
-    arg_value = {}
+    arg_value_dict = {}
+    targets = []
     if len(sys.argv) > 1:
         for arg in sys.argv[1:]:
-            key_value = [element.strip() for element in arg.split('=')]
-            if len(key_value) == 1:
-                key_value = ['dataset_dir', key_value[0]]
-                             
-            arg_value[key_value[0]] = key_value[1]
-        
-    # Default to project "data" directory
-    if not arg_value.get('dataset_dir'):
-        arg_value['dataset_dir'] = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'data')
+            if arg[0] == '-': # Leading ' ' indicates command line switch
+                key_value = [element.strip() for element in re.sub('-+', '', arg).split('=')]
+                if len(key_value) == 1:
+                    key_value = [key_value[0], True] # unary argument
+                                 
+                arg_value_dict[key_value[0]] = key_value[1]
+            else: # Not a command line switch - must be a target
+                targets.append(arg)
         
     print 'Parameters:'
-    for arg_key in arg_value.keys():
-        print '  %s = %s' % (arg_key, arg_value[arg_key])
-    
-    concept_hierarchy = ConceptHierarchy(lang=arg_value.get('lang'))   
-     
-    nc_path_list = sorted(glob(os.path.join(arg_value['dataset_dir'], '*.nc')))
+    for arg_key in arg_value_dict.keys():
+        print '  %s = %s' % (arg_key, arg_value_dict[arg_key])
+        
+    # Default to project "data" directory if no target specified
+    if not targets:
+        targets = [os.path.join(os.path.dirname(
+            os.path.dirname(os.path.abspath(__file__))), 'data')]
+        
+    print 'Target(s):'
+    for target in targets:
+        print '  %s' % target
 
+    # Build list of netCDF files
+    nc_path_list = []
+    for target in targets:
+        if os.path.isdir(target): # Read all netCDF files in specified directory
+            nc_path_list += sorted(glob(os.path.join(target, '*.nc')))
+        else:
+            nc_path_list += sorted(glob(target))
+
+    # Create ConceptHierarchy to build concept trees from URIs in targets
+    concept_hierarchy = ConceptHierarchy(lang=arg_value_dict.get('lang'), 
+                                         broader=True,
+                                         narrower=arg_value_dict.get('narrower'),
+                                         verbose=arg_value_dict.get('verbose')
+                                         )   
+     
     dataset_var_concept_dict = {}
     print '\nReading %s netCDF files' % len(nc_path_list)
     for nc_path in nc_path_list:
@@ -67,11 +91,13 @@ def main():
         print_concept_tree(top_concept, dataset_var_concept_dict)
         print
         
-    print 'Uncategorised (Missing URI)'
-    for dataset_variable in sorted([dataset_variable
+    uncategorised_list = sorted([dataset_variable
                               for dataset_variable in dataset_var_concept_dict.keys() 
-                              if not dataset_var_concept_dict[dataset_variable]]):
-        print '  ' + ':'.join([str(item) for item in dataset_variable])
+                              if not dataset_var_concept_dict[dataset_variable]])
+    if uncategorised_list:
+        print 'Uncategorised (Missing URI)'
+        for dataset_variable in uncategorised_list:
+            print '  ' + ':'.join([str(item) for item in dataset_variable])
 
 if __name__ == '__main__':
     main()
--- ncskosdump/concept_hierarchy.py
+++ ncskosdump/concept_hierarchy.py
@@ -18,19 +18,24 @@ class ConceptHierarchy(object):
         while "narrower" concepts are only updated as required as narrower concept URIs are encountered.
         Unresolved URIs will have an artificial top-level concept created.
         :param concept_uri: URI for concept.
+        :param verbose: Boolean flag to produce extra output
         :return dict containing information for concept read from cache or from SKOS query
             dict keys are: ['prefLabel', 'uri', 'altLabels', 'broader', 'narrower']
         """
-        
         if not concept_uri:
             return None
         
         concept = self.concept_registry.get(concept_uri)  # Check registry to see if we already have it
         
         if concept:
+            if self.verbose:
+                print 'Found concept in cache for URI %s' % concept_uri
             return concept
         
         try:
+            if self.verbose:
+                print 'Resolving concept_uri = %s' % concept_uri
+        
             concept_results = self.concept_fetcher.get_results(concept_uri)  # Look up URI
         except Exception as e:
             print 'WARNING: Unable to resolve URI %s: %s' % (concept_uri, e.message)
@@ -43,23 +48,30 @@ class ConceptHierarchy(object):
                 'uri': concept_uri,
                 'altLabels': [alt_label.strip() 
                               for alt_label in concept_results['skos__altLabels'].split(',') 
-                              if alt_label]
+                              if alt_label],
+                'broader': [],
+                'narrower': []
             }
-                                       
-            #=======================================================================
-            # for key, uri_list in {'narrower': concept_results.get('skos__narrower') or '', 
-            #                       'broader': concept_results.get('skos__broader') or ''
-            #                       }.iteritems():
-            #     concept[key] = [self.get_concept(uri.strip()) for uri in uri_list.split(',') if uri]
-            #=======================================================================
-            concept['broader'] = [self.get_concept(uri.strip()) for uri in (concept_results.get('skos__broader') or '').split(',') if uri]
-            concept['narrower'] = [] # Don't search for narrower concepts
-    
-            # Update narrower list in broader concept(s) as required
-            for broader_concept in concept['broader']:
-                if concept not in broader_concept['narrower']:
-                    broader_concept['narrower'].append(concept)
-        
+                
+            self.concept_registry[concept_uri] = concept
+            
+            # Recursively populate narrower & broader lists if required
+            if self.broader:
+                concept['broader'] += [self.get_concept(uri.strip()) for uri in (concept_results.get('skos__broader') or '').split(',') if uri]
+                
+                if not self.narrower:  # Update narrower list in broader concept(s) as required
+                    for broader_concept in concept['broader']:
+                        if concept not in broader_concept['narrower']:
+                            broader_concept['narrower'].append(concept)
+                 
+            if self.narrower:
+                concept['narrower'] += [self.get_concept(uri.strip()) for uri in (concept_results.get('skos__narrower') or '').split(',') if uri]
+
+                if not self.broader: # Update broader list in narrower concept(s) as required
+                    for narrower_concept in concept['narrower']:
+                        if concept not in narrower_concept['broader']:
+                            narrower_concept['broader'].append(concept)
+                                
         else: # Create "fake" (orphan & childless) concept for unresolved URIs
             label = 'Unresolved URI ' + os.path.basename(concept_uri)
             concept = {
@@ -70,8 +82,9 @@ class ConceptHierarchy(object):
                 'narrower': []
                 }
             
-        self.concept_registry[concept_uri] = concept
-
+            self.concept_registry[concept_uri] = concept
+            
+            
         return concept
     
     def get_top_concepts(self):
@@ -87,17 +100,27 @@ class ConceptHierarchy(object):
         '''
         return [concept for concept in self.concept_registry.values() if not concept['narrower']]
     
-    def __init__(self, initial_concept_uri=None, lang=None):
+    def __init__(self, initial_concept_uri=None, lang=None, broader=True, narrower=False, verbose=False):
         """
         Constructor for class ConceptHierarchy
-        :param initial_concept_uri: Optional initial URI from which to obtain "broader" concepts to build initial (linear) tree
+        Note: Need at least one of "narrower" or "broader" specified as True in order to build concept trees
+        :param initial_concept_uri: Optional initial URI from which to obtain "broader" concepts to build initial tree
         :param lang: Optional two-character ISO 639-1:200 language code. Defaults to 'en'
+        :param broader: Boolean flag dictating whether concept trees should be recursively populated up to top concept. 
+            Defaults to True
+        :param narrower: Boolean flag dictating whether concept trees should be recursively populated down to bottom concepts. 
+            Defaults to False
         """
+        assert narrower or broader, 'Need at least one of "broader" or "narrower" set to True in order to build concept trees'
+        
         self.lang = lang or 'en'
+        self.narrower = narrower
+        self.broader = broader
+        self.verbose = verbose
         
         skos_option_dict = {'altLabels': True, 
-                            'narrower': False, 
-                            'broader': True,
+                            'narrower': narrower, 
+                            'broader': broader,
                             'lang': lang
                             }           
         self.concept_fetcher = ConceptFetcher(skos_option_dict)
