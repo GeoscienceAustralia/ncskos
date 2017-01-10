285d88f74a74695f162c61cf04c9f7db73d3648f
Alex Ip <alex@trentham.net.au>
2017-01-11 08:06:31 +1100

    Shell script and batch files updated after refactoring

--- bin/ncskosdump.bat
+++ bin/ncskosdump.bat
@@ -3,4 +3,4 @@
 :: Written by Alex Ip 4/10/2016
 :: Example invocation: ncskosdump -hs %HOMEDRIVE%\%HOMEPATH%\git\ncskosdump\ncskosdump\test\sst.ltm.2000_skos.nc --skos lang=pl altLabels=True narrower=True broader=True
 
-python -m ncskosdump %*
+python -m ncskos.ncskosdump %*
